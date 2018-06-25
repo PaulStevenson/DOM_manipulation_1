d1708facac7e540e294b21732e211131b254bf4c
Paul <rpmstevenson@gmail.com>
2018-06-25 07:54:27 +0100

    mvp complete

--- js/app.js
+++ js/app.js
@@ -1,23 +1,30 @@
 document.addEventListener('DOMContentLoaded', () => {
-    console.log("JS loaded");
 
     const form = document.querySelector('#new-item-form');
+    form.addEventListener('submit', handleFormSubmit);
 
-    const handleFormSubmit = function (event) {
-        event.preventDefault();
-        console.log('form logged');
-
-        const form = event.target;
-        const runDate = form.runDate.value;
-        const distance = form.distance.value;
-        const duration = form.duration.value;
-        const difficulty = form.difficulty.value;
-
-        const result = document.querySelector('#running-list')
-        result.textContent = `${runDate} / ${distance}KM / ${duration} minutes / ${}`
-
-        form.reset()
-    }
-
-form.addEventListener('submit', handleFormSubmit);
+    const deleteAllButton = document.querySelector('#delete-all');
+    deleteAllButton.addEventListener('click', handleDeleteAllClick);
 });
+
+
+const handleFormSubmit = function (event) {
+    event.preventDefault();
+    console.log('form logged');
+
+    const form = event.target;
+    const runDate = form.runDate.value;
+    const distance = form.distance.value;
+    const duration = form.duration.value;
+    const difficulty = form.difficulty.value;
+
+    const result = document.querySelector('#running-list')
+    result.textContent = `You ran ${distance}km on ${runDate}. It took you ${duration} minutes, which you found to be ${difficulty}.`
+
+    form.reset()
+}
+
+const handleDeleteAllClick = function (event) {
+    const runningList = document.querySelector('#running-list');
+    runningList.innerHTML = '';
+};
